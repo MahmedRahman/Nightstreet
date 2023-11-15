86b3dcfee8da5379783755ea944d398c648aed3b
atpfreelancer <atpfreelancer@gmail.com>
2023-11-15 11:49:01 +0200

    code

--- lib/app/modules/home_page_products/views/home_page_products_view.dart
+++ lib/app/modules/home_page_products/views/home_page_products_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:krzv2/app/modules/home_page/controllers/home_page_product_categories_controller.dart';
 import 'package:krzv2/app/modules/home_page_products/controllers/home_page_exclusive_offers_product_controller.dart';
@@ -17,29 +18,65 @@ import 'package:krzv2/component/views/slider_view.dart';
 import 'package:krzv2/extensions/widget.dart';
 import 'package:krzv2/routes/app_pages.dart';
 import 'package:krzv2/services/auth_service.dart';
+import 'package:krzv2/utils/app_colors.dart';
 import 'package:krzv2/utils/app_dimens.dart';
 import 'package:krzv2/utils/app_spacers.dart';
 import 'package:krzv2/web_serives/model/api_response_model.dart';
 import 'package:krzv2/web_serives/web_serives.dart';
 import '../controllers/home_page_products_controller.dart';
 
-class MarketController extends GetxController with StateMixin<List> {
+class MarketController extends GetxController with StateMixin<List>, ScrollMixin {
   @override
   void onInit() {
     getMarket();
     super.onInit();
   }
 
-  void getMarket() async {
+  Future getMarket() async {
     ResponseModel responseModel = await WebServices().getMarket();
 
     print(responseModel.data["success"].toString());
     if (responseModel.data["success"]) {
+      if (responseModel.data["data"]["data"].isEmpty) {
+        change([], status: RxStatus.empty());
+        return;
+      }
       change(responseModel.data["data"]["data"], status: RxStatus.success());
+      
+      isPagination = responseModel.data['data']['pagination']['is_pagination'] as bool;
+
       return;
     }
     change(null, status: RxStatus.error());
     return;
+  }
+
+  int currentPage = 1;
+  bool isPagination = false;
+  @override
+  Future<void> onEndScroll() async {
+    if (isPagination == false) return;
+    currentPage++;
+
+    Get.dialog(
+      const Center(
+        child: SpinKitCircle(
+          color: AppColors.mainColor,
+          size: 70,
+        ),
+      ),
+    );
+
+    await getMarket();
+
+    Get.back();
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> onTopScroll() {
+    // TODO: implement onTopScroll
+    throw UnimplementedError();
   }
 }
 
