4fddd1c9119c92bb9c9fe9e15fe9e906046bd756
a.ehab <a.ehab@houya.io>
2023-11-15 12:49:52 +0200

    market favorite

--- lib/app/modules/home_page_products/views/home_page_products_view.dart
+++ lib/app/modules/home_page_products/views/home_page_products_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
+import 'package:krzv2/app/modules/favorite/controllers/market_favorite_controller.dart';
 import 'package:krzv2/app/modules/home_page/controllers/home_page_product_categories_controller.dart';
 import 'package:krzv2/app/modules/home_page_products/controllers/home_page_exclusive_offers_product_controller.dart';
 import 'package:krzv2/app/modules/home_page_products/controllers/home_page_most_seller_product_controller.dart';
@@ -10,6 +11,7 @@ import 'package:krzv2/app/modules/store/view/store_view.dart';
 import 'package:krzv2/component/views/app_bar_search_view.dart';
 import 'package:krzv2/component/views/cards/market_card_view.dart';
 import 'package:krzv2/component/views/cards/service_card_view.dart';
+import 'package:krzv2/component/views/custom_dialogs.dart';
 import 'package:krzv2/component/views/home_categories_list_view.dart';
 import 'package:krzv2/component/views/notification_icon_view.dart';
 import 'package:krzv2/component/views/scaffold/base_scaffold.dart';
@@ -25,7 +27,8 @@ import 'package:krzv2/web_serives/model/api_response_model.dart';
 import 'package:krzv2/web_serives/web_serives.dart';
 import '../controllers/home_page_products_controller.dart';
 
-class MarketController extends GetxController with StateMixin<List>, ScrollMixin {
+class MarketController extends GetxController
+    with StateMixin<List>, ScrollMixin {
   @override
   void onInit() {
     getMarket();
@@ -42,8 +45,9 @@ class MarketController extends GetxController with StateMixin<List>, ScrollMixin
         return;
       }
       change(responseModel.data["data"]["data"], status: RxStatus.success());
-      
-      isPagination = responseModel.data['data']['pagination']['is_pagination'] as bool;
+
+      isPagination =
+          responseModel.data['data']['pagination']['is_pagination'] as bool;
 
       return;
     }
@@ -90,14 +94,16 @@ class HomePageProductsView extends GetView<HomePageProductsController> {
   final sliderController = Get.find<HomePageProductSliderController>();
   final mostSelleerProductController = Get.put(MostSelleerProductController());
   final cartController = Get.find<ShoppingCartController>();
-  final exclusiveOffersProductController = Get.put(ExclusiveOffersProductController());
+  final exclusiveOffersProductController =
+      Get.put(ExclusiveOffersProductController());
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
       appBar: AppBarSerechView(
         placeHolder: 'ما الذي تريد البحث عنه ؟',
         actions: [
-          if (authController.isLoggedIn || authController.isGuestUser) ShoppingCartIconView(),
+          if (authController.isLoggedIn || authController.isGuestUser)
+            ShoppingCartIconView(),
           if (authController.isLoggedIn) NotificationIconView(),
           AppSpacers.width20,
         ],
@@ -173,16 +179,35 @@ class HomePageProductsView extends GetView<HomePageProductsController> {
                   padding: AppDimension.appPadding,
                   itemCount: data!.length,
                   itemBuilder: (context, index) {
-                    return MarketCardView(
-                      imageUrl: data[index]["image"].toString(),
-                      name: data[index]["name"].toString(),
-                      onFavoriteTapped: () {},
-                      onTapped: () {
-                        Get.to(MarketPage(data[index]));
+                    return GetBuilder<MarketFavoriteController>(
+                      init: MarketFavoriteController(),
+                      builder: (favController) {
+                        return MarketCardView(
+                          imageUrl: data[index]["image"].toString(),
+                          name: data[index]["name"].toString(),
+                          onFavoriteTapped: () {
+                            if (Get.put(
+                                    AuthenticationController().isLoggedIn) ==
+                                false) {
+                              return AppDialogs.showToast(
+                                  message: 'الرجاء تسجيل الدخول');
+                            }
+
+                            favController.addRemoveMarketFromFavorite(
+                              branchId: data[index]["id"],
+                            );
+                          },
+                          onTapped: () {
+                            Get.to(MarketPage(data[index]));
+                          },
+                          isFavorite:
+                              favController.marketsFavoriteIds.value!.contains(
+                            data[index]["id"],
+                          ),
+                        ).paddingOnly(
+                          bottom: 10,
+                        );
                       },
-                      isFavorite: false,
-                    ).paddingOnly(
-                      bottom: 10,
                     );
                   },
                 );
