be388cff6bbb8eba9821e2e4983090db12783c76
atpfreelancer <atpfreelancer@gmail.com>
2023-11-20 04:32:53 +0200

    test

---
+++ lib/core/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static Color KmainColor = Color(0xff490059);
+  static Color KsecondaryColor = Color(0xffF26404);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_night_street/screen/home_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,11 +6,22 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NightStreet',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
 }
