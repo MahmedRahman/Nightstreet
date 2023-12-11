9deca2262f7e5d01a075790b6298440b8dd784b2
a.ehab <a.ehab@houya.io>
2023-12-11 16:50:16 +0200

    select date

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -1,89 +1,165 @@
 import 'package:flutter/material.dart';
 import 'package:easy_date_timeline/easy_date_timeline.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/utils.dart';
+import 'package:krzv2/component/views/cashed_network_image_view.dart';
+import 'package:krzv2/component/views/custom_back_button_component.dart';
 import 'package:krzv2/utils/app_colors.dart';
+import 'package:krzv2/utils/app_dimens.dart';
+import 'package:krzv2/utils/app_spacers.dart';
 
-class MyTestScreen extends StatelessWidget {
-  const MyTestScreen({super.key});
+class MyTestScreen extends GetView {
+  MyTestScreen();
 
+  final focusDate = Rx<DateTime?>(DateTime.now());
+  final EasyInfiniteDateTimelineController _controller =
+      EasyInfiniteDateTimelineController();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: EasyDateTimeLine(
-                initialDate: DateTime.now(),
-                onDateChange: (selectedDate) {
-                  //`selectedDate` the new date selected.
-                },
-                headerProps: const EasyHeaderProps(
-                  monthPickerType: MonthPickerType.switcher,
-                  selectedDateFormat: SelectedDateFormat.fullDateDMonthAsStrY,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          CustomBackButton(),
+          const SizedBox(width: 20),
+        ],
+      ),
+      body: Obx(
+        () => Padding(
+          padding: AppDimension.appPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'اختر الوقت',
+                style: TextStyle(
+                  color: AppColors.blackColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                dayProps: const EasyDayProps(
-                  // You must specify the width in this case.
-                  width: 124.0,
-                ),
-                disabledDates: [DateTime.now()],
-                itemBuilder: (context, dayNumber, dayName, monthName, fullDate, isSelected) {
-                  return Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: isSelected ? AppColors.mainColor : Colors.transparent,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.black,
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.all(25),
-                          child: Text(
-                            "$dayNumber",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: isSelected ? Colors.white : Colors.black,
-                              fontFamily: "effra",
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        "$dayName",
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  );
-                },
+              ),
+              AppSpacers.height25,
+              doctorInfo(
+                name: 'Mohammed Shams',
+                image:
+                    "https://cdn.vectorstock.com/i/preview-2x/44/06/doctor-icon-circle-vector-15604406.webp",
+              ),
+              AppSpacers.height25,
+              EasyInfiniteDateTimeLine(
+                controller: _controller,
                 activeColor: const Color(0xffB04759),
                 locale: "ar",
+                onDateChange: (selectedDate) {
+                  focusDate.value = selectedDate;
+                },
+                timeLineProps: EasyTimeLineProps(
+                  hPadding: 0.0,
+                  margin: EdgeInsets.zero,
+                ),
+                disabledDates: [DateTime.now()],
+                focusDate: focusDate.value,
+                firstDate: DateTime.now(),
+                lastDate: DateTime.now().add(
+                  Duration(days: 365 * 2),
+                ),
+                itemBuilder: itemBuilder,
               ),
-            ),
-            Divider(),
-            Row(
-              children: [
-                Text("2:00"),
-                Text("pm"),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: Colors.black,
-            ),
-          ],
+              Divider(),
+              Expanded(
+                child: ListView.separated(
+                  itemCount: 50,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Text(
+                          "${index + 1}:00 PM",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  separatorBuilder: (_, __) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  ListTile doctorInfo({
+    required String name,
+    required String image,
+  }) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Container(
+        width: 60.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: CashedNetworkImageView(
+          imageUrl: image,
+          boxFit: BoxFit.cover,
+        ),
+      ),
+      title: Text(name),
+    );
+  }
+
+  Widget itemBuilder(_, dayNumber, dayName, __, ___, isSelected) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: isSelected ? AppColors.mainColor : Colors.transparent,
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: isSelected ? Colors.transparent : Colors.grey,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "$dayNumber",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: isSelected ? Colors.white : Colors.black,
+                fontFamily: "effra",
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          "$dayName",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ],
     );
   }
 }
